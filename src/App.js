fc4e7b4a2236156712e60751ec0f56cc66312023
Hesbon Angwenyi <hesbonmanyinsa96@gmail.com>
2025-12-11 11:59:16 +0300

    updated

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import RealtimeChart from "./components/RealtimeChart";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Real-Time Analytics Dashboard</h1>
+      <RealtimeChart />
+    </div>
+  );
+}
